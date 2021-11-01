f2ba66ca552d2d0eb8197188340efee040aeef22
Tanvir Anzum <shihabahmed133075@gmail.com>
2021-11-01 20:25:41 +0600

    a

--- index.js
+++ index.js
@@ -21,108 +21,101 @@ app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Heroku works!')
 })
 
-async function run(){
- try{
-  await client.connect()
-  const EventsCollection = client.db('volunteerNetwork').collection('events')
-  const ordersCollection = client.db('volunteerNetwork').collection('orders')
-  
+async function run() {
+  try {
+    await client.connect()
+    const EventsCollection = client.db('volunteerNetwork').collection('events')
+    const ordersCollection = client.db('volunteerNetwork').collection('orders')
 
-  // add Events
-  app.post('/addEvent', async (req, res) => {
-    console.log(req.body)
-    const result = await EventsCollection.insertOne(req.body)
-    console.log(result)
-    res.send(result)
-  })
-
-  // get search events
-  app.get('/searchEvent', async (req, res) => {
-    const result = await EventsCollection.find({
-      title: { $regex: req.query.search },
-    }).toArray()
-    res.send(result)
-    console.log(result)
-  })
-
-  // add volunteer
-
-  app.post('/orders', async (req, res) => {
-    console.log(req.body)
-    console.log('dekhi')
-    const result = await ordersCollection.insertOne(req.body)
-    res.send(result)
-  })
-
-  // get all volunteer
-
-  // get all events
-
-  app.get('/allEvents', async (req, res) => {
-    const result = await EventsCollection.find({}).toArray()
-    res.send(result)
-  })
-  app.get('/orders', async (req, res) => {
-    const result = await ordersCollection.find({}).toArray()
-    res.send(result)
-  })
-
-  // delete event
-
-  app.delete('/deleteEvent/:id', async (req, res) => {
-    console.log(req.params.id)
-    console.log('ses eta')
-    const result = await EventsCollection.deleteOne({
-      _id: ObjectId(req.params.id),
+    // add Events
+    app.post('/addEvent', async (req, res) => {
+      console.log(req.body)
+      const result = await EventsCollection.insertOne(req.body)
+      console.log(result)
+      res.send(result)
     })
-    res.send(result)
-  })
-  app.delete('/deleteOrder/:id', async (req, res) => {
-    
-    const result = await ordersCollection.deleteOne({
-      _id: ObjectId(req.params.id),
+
+    // get search events
+    app.get('/searchEvent', async (req, res) => {
+      const result = await EventsCollection.find({
+        title: { $regex: req.query.search },
+      }).toArray()
+      res.send(result)
+      console.log(result)
     })
-    console.log(result)
-    res.send(result)
-  })
- 
 
-  // my events
+    // add volunteer
 
-  app.get('/allEvents/:email', async (req, res) => {
-    const result = await EventsCollection.find({
-      email: req.params.email,
-    }).toArray()
-    res.send(result)
-  })
-  app.get('/orders/:email', async (req, res) => {
-    const result = await ordersCollection.find({
-      email: req.params.email,
-    }).toArray()
-    res.send(result)
-  })
-  app.get('/singleItem/:id', async (req, res) => {
-    const id = req.params.id
-    const query = {_id: ObjectId(req.params.id)}
-    const result = await EventsCollection.findOne(
-      query)
-    res.json(result)
-  })
+    app.post('/orders', async (req, res) => {
+      console.log(req.body)
+      console.log('dekhi')
+      const result = await ordersCollection.insertOne(req.body)
+      res.send(result)
+    })
 
- }
- finally{
- }
+    // get all volunteer
+
+    // get all events
+
+    app.get('/allEvents', async (req, res) => {
+      const result = await EventsCollection.find({}).toArray()
+      res.send(result)
+    })
+    app.get('/orders', async (req, res) => {
+      const result = await ordersCollection.find({}).toArray()
+      res.send(result)
+    })
+
+    // delete event
+
+    app.delete('/deleteEvent/:id', async (req, res) => {
+      console.log(req.params.id)
+      console.log('ses eta')
+      const result = await EventsCollection.deleteOne({
+        _id: ObjectId(req.params.id),
+      })
+      res.send(result)
+    })
+    app.delete('/deleteOrder/:id', async (req, res) => {
+      const result = await ordersCollection.deleteOne({
+        _id: ObjectId(req.params.id),
+      })
+      console.log(result)
+      res.send(result)
+    })
+
+    // my events
+
+    app.get('/allEvents/:email', async (req, res) => {
+      const result = await EventsCollection.find({
+        email: req.params.email,
+      }).toArray()
+      res.send(result)
+    })
+    app.get('/orders/:email', async (req, res) => {
+      const result = await ordersCollection
+        .find({
+          email: req.params.email,
+        })
+        .toArray()
+      res.send(result)
+    })
+    app.get('/singleItem/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: ObjectId(req.params.id) }
+      const result = await EventsCollection.findOne(query)
+      res.json(result)
+    })
+  } finally {
+  }
 }
 run().catch(console.dir)
- 
- 
 
-  // add addVolunteer
+// add addVolunteer
 
-  // get all volunteer
+// get all volunteer
 
- 
 app.listen(process.env.PORT || port)
